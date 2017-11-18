be3eab6cec780b2dd190b27395ffb64bfc64ddb2
Christoph Dorn <christoph@christophdorn.com>
2017-11-17 18:27:25 -0800

    wip

--- _#_org.bashorigin_#_s1.sh
+++ _#_org.bashorigin_#_s1.sh
@@ -16,45 +16,69 @@ function PRIVATE_ensure {
         fi
 
         pushd "$baseDir" > /dev/null
-            BO_run_node --eval '
-                const FS = require("fs");
 
-                const setName = process.argv[1];
-                const declarations = JSON.parse(process.argv[2]);
+            if [ ! -e ".installed.log" ] || ! grep -q "$__ARG1__" ".installed.log"; then
 
-                if (!declarations[setName]) {
-                    console.error("ERROR: Set name \"" + setName + "\" not found!");
-                    process.exit(1);
-                }
+                BO_run_recent_node --eval '
+                    const FS = require("fs");
 
-                function deepObjectExtend (target, source) {
-                    for (var prop in source) {
-                        if (source.hasOwnProperty(prop)) {
-                            if (target[prop] && typeof source[prop] === "object") {
-                                deepObjectExtend(target[prop], source[prop]);
-                            }
-                            else {
-                                target[prop] = source[prop];
+                    const setName = process.argv[1];
+                    const declarations = JSON.parse(process.argv[2]);
+
+                    if (!declarations[setName]) {
+                        console.error("ERROR: Set name \"" + setName + "\" not found!");
+                        process.exit(1);
+                    }
+
+                    function deepObjectExtend (target, source) {
+                        for (var prop in source) {
+                            if (source.hasOwnProperty(prop)) {
+                                if (target[prop] && typeof source[prop] === "object") {
+                                    deepObjectExtend(target[prop], source[prop]);
+                                }
+                                else {
+                                    target[prop] = source[prop];
+                                }
                             }
                         }
+                        return target;
                     }
-                    return target;
+
+                    var descriptor = {};
+                    if (FS.existsSync("package.json")) {
+                        descriptor = JSON.parse(FS.readFileSync("package.json", "utf8"));
+                    }
+                    // TODO: Instead of pummeling existing versions use the highest one.
+                    descriptor[setName] = deepObjectExtend(descriptor[setName] || {}, declarations[setName]);
+
+                    // TODO: If no version specified for name use latest version.
+
+                    if (process.env.BO_VERBOSE) {
+                        console.log("[github.com~pinf-it~it.pinf.org.npmjs] descriptor:", descriptor);
+                    }
+
+                    FS.writeFileSync("package.json", JSON.stringify(descriptor, null, 4), "utf8");
+                ' "$1" "$__ARG1__"
+
+
+                echo "TEST_MATCH_IGNORE>>>"
+
+                rm -f package-lock.json || true
+
+                function installError {
+                    echo "ERROR while installing package '$(pwd)' using 'BO_run_npm install'!"
+                    exit 1
                 }
 
-                var descriptor = {};
-                if (FS.existsSync("package.json")) {
-                    descriptor = JSON.parse(FS.readFileSync("package.json", "utf8"));
-                }
-                // TODO: Instead of pummeling existing versions use the highest one.
-                descriptor[setName] = deepObjectExtend(descriptor[setName] || {}, declarations[setName]);
+                # TODO: Checksum descriptor and write '.installed' file
+                # TODO: Suppress WARN messages
+                BO_run_npm install || installError
 
-                // TODO: If no version specified for name use latest version.
+                echo "<<<TEST_MATCH_IGNORE"
 
-                FS.writeFileSync("package.json", JSON.stringify(descriptor, null, 4), "utf8");
-            ' "$1" "$__ARG1__"
 
-            # TODO: Checksum descriptor and write '.installed' file
-            BO_run_npm install
+                echo "$__ARG1__" >> ".installed.log"
+            fi
 
         popd > /dev/null
 
@@ -65,8 +89,6 @@ function PRIVATE_ensure {
 }
 
 
-echo "TEST_MATCH_IGNORE>>>"
 pushd "$__CALLER_DIRNAME__" > /dev/null
     PRIVATE_ensure dependencies
 popd > /dev/null
-echo "<<<TEST_MATCH_IGNORE"
